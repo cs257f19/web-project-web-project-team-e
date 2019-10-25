49f9c8fb88c283b19a3d13b5a6c616bf3e7cdba9
loye16 <37096340+loye16@users.noreply.github.com>
2019-10-25 16:43:14 -0500

    Adding Random Project Method

--- QueryCode/datasource.py
+++ QueryCode/datasource.py
@@ -12,14 +12,44 @@ class DataSource:
     	self.numberOfProjects = 378661
         pass
 
-    #our methods/ideas
+    def connect(user, password):
+	'''
+	Establishes a connection to the database with the following credentials:
+		user - username, which is also the name of the database
+		password - the password for this database on perlman
 
-    #maybe working implementation - Elisa
-    def getRandomProject():
-    	cursor = connection.cursor()
-    	query = "SELECT ID FROM ksdata ORDER BY RAND() LIMIT 1"
-    	cursor.execute(query)
-		return cursor.fetchall()
+	Returns: a database connection.
+
+	Note: exits if a connection cannot be established.
+	Note: Code written by Amy Csizmar Dalal
+	'''
+	try:
+		connection = psycopg2.connect(database=user, user=user, password=password)
+	except Exception as e:
+		print("Connection error: ", e)
+		exit()
+	return connection
+
+    def getRandomProject(connection):
+        '''
+        Gives the name of a random project in the kickstarter dataset 
+        
+        PARAMETERS:
+            connection - the connection to the database
+            
+        RETURNS:
+            the ID of a random project, once the data is clean, we will produce the name of the project
+        '''
+        
+        try:
+            cursor = connection.cursor()
+            query = "SELECT ID FROM ksdata ORDER BY RAND() LIMIT 1"
+            cursor.execute(query)
+            return cursor.fetchall()
+
+        except Exception as e:
+            print ("Something went wrong when executing the query: ", e)
+            return Nonecursor = connection.cursor()
 
     #ben - how many projects where there in the us
     #IMPLEMENT
@@ -30,7 +60,7 @@ class DataSource:
         PARAMETERS:
             nameOfVariable - the variable of the project we are counting from.
             variableCondition - an attribute of the main variable (i.e category, country, currency)
-			varibaleConditionToMeet - the condition that needs to be met for the project to be counted
+	    varibaleConditionToMeet - the condition that needs to be met for the project to be counted
 
 
         RETURN:
