313be66f62ed985183c793f006085cb12dd5cd33
bensantos <santosb@perlman.mathcs.carleton.edu>
2019-10-25 20:06:19 -0500

    la

---
+++ Data/createtable.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS ksdata;
+CREATE TABLE ksdata (
+  id real,
+  category text,
+  main_category text,
+  currency text,
+  deadline date,
+  goal real,
+  launched date,
+  total_days real,
+  pledged real,
+  state text,
+  backers real,
+  country text,
+  usd_pledged_real real,
+  usd_goal_real real
+);
