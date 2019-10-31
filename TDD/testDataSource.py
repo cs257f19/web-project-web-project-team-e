22a1f8dea0e0f6530bc5e7bd96afa9323e60130d
bensantos <santosb@carleton.edu>
2019-10-30 21:08:00 -0500

    Update testDataSource.py

--- TDD/testDataSource.py
+++ TDD/testDataSource.py
@@ -1,9 +1,14 @@
 import unittest
+from datasource import*
 
+class DataSourceTester(unittest.TestCase):
+    def setUp(self):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+        self.ds = datasource()
+    def test_correct_parameters(self):
+        user = 'santosb'
+        password = 'books347winter'
+        self.assertTrue(self.ds.getCountOfVariableSuccess(connection, nameOfVariable, variableConditionToMeet))
 
 
 if __name__ == '__main__':
