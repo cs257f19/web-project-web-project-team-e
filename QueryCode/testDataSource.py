85bfb060e7bec0a9dcf94fbcf1d7f6e42452abbe
bensantos <santosb@carleton.edu>
2019-10-31 12:17:47 -0500

    Update testDataSource.py

--- QueryCode/testDataSource.py
+++ QueryCode/testDataSource.py
@@ -8,12 +8,18 @@ class DataSourceTester(unittest.TestCase):
         self.ds = DataSource()
         self.connection = self.ds.connect()
 
-    def test_correct_parameters(self):
+    def test_number_within_possible_range(self):
         nameOfVariable = 'country'
         variableConditionToMeet = 'US'
         input = self.ds.getCountOfVariableSuccess(self.connection, nameOfVariable, variableConditionToMeet)
         #zero projects and total number of projects
         self.assertGreaterEqual(input,0) and self.assertLessEqual(input, 378661)
+
+    def test_if_parameter_exists_in_table(self):
+        nameOfVariable = 'potato'
+        variableConditionToMeet = 'US'
+        input = self.ds.getCountOfVariableSuccess(self.connection, nameOfVariable, variableConditionToMeet)
+        self.assertFalse(input)
 
 
 if __name__ == '__main__':
