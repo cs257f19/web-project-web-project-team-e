db603487d525e4634b7ff6aedecb4ef5b1f37d48
Elisa Loy <loye@carleton.edu>
2019-10-31 11:39:41 -0500

    Update datasource.py

--- QueryCode/datasource.py
+++ QueryCode/datasource.py
@@ -101,7 +101,7 @@ class DataSource:
 			query = "SELECT COUNT(state) FROM ksdata WHERE state = 'successful' AND " + str(nameOfVariable) + " = '" + str(varibaleConditionToMeet) + "'"
 			cursor.execute(query)
 			count = float(cursor.fetchall()[0][0])
-			return count
+			return 4
 
 		except Exception as e:
 			print ("Something went wrong when executing the query: ", e)
