aea9d3611df33303da16a2dbbf127b44810a7f3c
Elisa Loy <loye@carleton.edu>
2019-10-31 11:31:11 -0500

    Update datasource.py

--- QueryCode/datasource.py
+++ QueryCode/datasource.py
@@ -82,7 +82,7 @@ class DataSource:
 			print ("Something went wrong when executing the query: ", e)
 			return connection.cursor()
 
-	def getCountofVariableSuccess(connection, nameOfVariable, varibaleConditionToMeet):
+	def getCountOfVariableSuccess(connection, nameOfVariable, varibaleConditionToMeet):
 		'''
         Returns the count (an integer) of all projects of one variable that meet a certain condition AND were sucessful.
 
