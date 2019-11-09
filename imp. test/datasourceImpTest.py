d426cf5be7bc1efb0ce520bdad43d66e14526dbe
Elisa Loy <loye@carleton.edu>
2019-11-08 21:06:54 -0600

    Update datasourceImpTest.py

--- imp. test/datasourceImpTest.py
+++ imp. test/datasourceImpTest.py
@@ -344,11 +344,12 @@ def main():
     connection = ds.connect()
     print(str(ds.getListOfAllProjectsOfOneCategory(connection,'Crafts','Printing')))
     print("The total number of projects is:" + str(ds.getNumberOfProjects(connection)))
-	print("A random project is:" + str(ds.getRandomProject(connection)))
-	print("The minimum value of the 'backers' is:" + str(ds.getMinimumValueOfVariable(connection,'backers')))
-	print("The average days for a project is: " + str(ds.getAverageOfVariable(connection, 'total_days')))
-	print("The proportion of Music projects that were succesful is: " + str(ds.getProportionOfSuccess(connection, 'main_category', 'Music')))
+    print("A random project is:" + str(ds.getRandomProject(connection)))
+    print("The minimum value of the 'backers' is:" + str(ds.getMinimumValueOfVariable(connection,'backers')))
+    print("The average days for a project is: " + str(ds.getAverageOfVariable(connection, 'total_days')))
+    print("The proportion of Music projects that were succesful is: " + str(ds.getProportionOfSuccess(connection, 'main_category', 'Music')))
     connection.close()
-          
-          
+    
+    
+    
 main()
