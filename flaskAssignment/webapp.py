dbd98ae7b6084a448ca6869ed00169312a97a42a
bensantos <santosb@carleton.edu>
2019-11-11 10:59:07 -0600

    Update webapp.py

--- flaskAssignment/webapp.py
+++ flaskAssignment/webapp.py
@@ -2,7 +2,8 @@ import flask
 from flask import render_template
 import json
 import sys
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
