4967fb728fd7757e595f71aaa5c4809bc69f5839
bensantos <santosb@carleton.edu>
2019-11-11 10:58:16 -0600

    Update webapp.py

--- flaskAssignment/webapp.py
+++ flaskAssignment/webapp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+import flask
+from flask import render_template
 import json
 import sys
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
