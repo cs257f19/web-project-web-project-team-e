ea208bd057e5797f11c0a992bafda776cc4f10db
bensantos <santosb@carleton.edu>
2019-11-11 00:16:59 -0600

    Update webapp.py

--- flaskAssignment/webapp.py
+++ flaskAssignment/webapp.py
@@ -1,5 +1,4 @@
-import flask
-from flask import render_template
+from flask import Flask, render_template, request
 import json
 import sys
 
@@ -10,4 +9,10 @@ def form():
     return render_template('form.html')
 
 if __name__ == "__main__":
-    app.run()
+    if len(sys.argv) != 3:
+        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+        exit()
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    app.run(host=host, port=port)
